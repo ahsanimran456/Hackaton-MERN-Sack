4aad82d5a10aab5f5f18086c955c7f434bb0563c
Ahsan Imran <111527387+ahsanimran456@users.noreply.github.com>
2023-02-04 11:55:36 -0700

    Add files via upload
    add product

---
+++ Router/AddProduct.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import express from "express";
+const app = express()
+const productSchema = mongoose.Schema(
+    {
+        ProductName: { type: String, required: true },
+        Category: { type: String, required: true, },
+        Price: { type: Number, required: true },
+        Description: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+const Products = mongoose.model("Products", productSchema);
+
+function AddProducts(req, res) {
+    const body = req.body
+    console.log(body)
+    if (body.ProductName && body.Category && body.Price && body.Description) {
+        Products.findOne({ ProductName: body.ProductName }, async (err, product) => {
+            if (!err) {
+                // ........when product exits .....//
+                if (product) {
+                    console.log("user exist is db ")
+                    res.send({ message: " Product is Already token " })
+                }
+                // ........when Product does not exits .....//
+                else {
+                    try {
+                        const addproductDb = await Products.create({
+                            ProductName: body.ProductName,
+                            Category: body.Category,
+                            Price: body.Price,
+                            Description: body.Description,
+                        })
+                        res.status(200).send({ message: 'Product Added Successfully', data: addproductDb })
+                    }
+                    catch (err) {
+                        console.log(err);
+                        res.send({ message: 'Server Error' })
+                    }
+                }
+            }
+        })
+    } else {
+        res.send({ Message: "Required parameter in missing" })
+    }
+}
+
+export default AddProducts;
--- Router/Router.js
+++ Router/Router.js
@@ -2,6 +2,7 @@ import express from "express";
 import {Adduser} from "./Adduser.js";
 import Getusers from "./Getusers.js";
 import Deleteuser from "./Deleteuser.js";
+import AddProducts from "./AddProduct.js";
 import mongoose from 'mongoose';
 
 
@@ -10,5 +11,6 @@ const Router = express.Router();
 Router.post("/adduser",Adduser);
 Router.delete("/deleteuser/:id",Deleteuser)
 Router.get("/getusers",Getusers);
+Router.post("/addproducts",AddProducts)
 
 export default Router
